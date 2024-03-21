21729257bfb86a89fb1b1632b4e761eda62ad4df
datadiego <juandiegomariscal@gmail.com>
2024-03-21 09:19:52 +0100

    update: nuevo ejercicio

---
+++ ejercicios/libro.js
@@ -0,0 +1,8 @@
+//Crea una función que cuente el número de veces que aparece una palabra en un texto.
+//Asegurate de filtrar caracteres especiales en palabras, como las que terminan en "." o comienzan con "¿".
+//Utiliza la variable libro para contar el número de veces que aparece la palabra "señor".
+const libro = require("./biblia.txt")
+
+const cuentaPalabras = (str) =>
+
+module.exports = cuentaPalabras
