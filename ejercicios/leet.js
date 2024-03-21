46a7639b8f2525d75f77fd70cb4c188c722a815c
Javi <javiercamarerolopez@gmail.com>
2024-03-21 12:50:29 +0100

    feat: funcion leet terminada

--- ejercicios/leet.js
+++ ejercicios/leet.js
@@ -11,6 +11,27 @@
 // hola => h0l4
 //no puedes usar objetos, ni ifs, ni bucles.
 
-const leet = () => 
+const leet = (str) => {
+    const myMap = new Map();
+    myMap.set("o", "0");
+    myMap.set("O", "0");
+    myMap.set("i", "1");
+    myMap.set("I", "1");
+    myMap.set("e", "3");
+    myMap.set("E", "3");
+    myMap.set("a", "4");
+    myMap.set("A", "4");
+    myMap.set("s", "5");
+    myMap.set("S", "5");
+    myMap.set("t", "7");
+    myMap.set("T", "7");
+    myMap.set("b", "8");
+    myMap.set("B", "8");
+    myMap.set("g", "9");
+    myMap.set("G", "9");
+
+    return str.split("").map(caracter => myMap.has(caracter) ? myMap.get(caracter) : caracter).join("");
+}
+console.log(leet("Hola mundo"));
 
 module.exports = leet
