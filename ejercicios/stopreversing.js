dd710bc00e9e2e87037448cecf2b21019508c649
Arkant04 <delacorte19@gmail.com>
2024-03-21 10:58:44 +0100

    MOD: se acaba el ejercicio de stopreversing

--- ejercicios/stopreversing.js
+++ ejercicios/stopreversing.js
@@ -1,9 +1,23 @@
-//Laz una función que vuelva del revés strings
+//Haz una función que vuelva del revés strings
 //La función debe devolver un string como el original, pero con el orden al inverso
 // Hola => aloH
 // Lorem ipsum => muspi meroL
 //Sólo puedes usar una linea de código
 
-const reverse = () => 
+//const reverse = () => 
+
+function reverse(str){
+// convertir string a array
+// hacer bucle q vaya de str.length a 0 
+// asignar el valor q estamos recorriendo 
+const arr = str.split("") // ("H", "O", "L", "A")
+const resultado = []
+for(let i = str.length-1; i >= 0; i--){
+  resultado.push(arr[i])
+}
+ return resultado.join("")
+}
+const a = reverse("hola") 
+console.log(a)
 
 module.exports = reverse
--- test/stopreversing.test.js
+++ test/stopreversing.test.js
@@ -1,25 +1,25 @@
-const reversing = require('../ejercicios/stopreversing');
+const reverse = require('../ejercicios/stopreversing');
 
 test('Reversing "hola" => "aloh"', () => {
-    expect(reversing('hola')).toBe('aloh');
+    expect(reverse('hola')).toBe('aloh');
     });
 
 test('Reversing "mundo" => "odnum"', () => {
-    expect(reversing('mundo')).toBe('odnum');
+    expect(reverse('mundo')).toBe('odnum');
     });
 
 test('Reversing "hola mundo" => "odnum aloh"', () => {
-    expect(reversing('hola mundo')).toBe('odnum aloh');
+    expect(reverse('hola mundo')).toBe('odnum aloh');
     });
 
 test('Reversing "donde caemos gente" => "etneg someac ednod"', () => {
-    expect(reversing('donde caemos gente')).toBe('etneg someac ednod');
+    expect(reverse('donde caemos gente')).toBe('etneg someac ednod');
     });
 
 test('Reversing "Lorem ipsun dolor sit amet" => "tema tis rolod nuspi meroL"', () => {
-    expect(reversing('Lorem ipsun dolor sit amet')).toBe('tema tis rolod nuspi meroL');
+    expect(reverse('Lorem ipsun dolor sit amet')).toBe('tema tis rolod nuspi meroL');
     });
 
 test('Reversing 123456789 => 987654321', () => {
-    expect(reversing('123456789')).toBe('987654321');
+    expect(reverse('123456789')).toBe('987654321');
     });
