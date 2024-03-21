7a2aa56f8fdc205aaa6c7fee1dad44a46a140c64
17alma41 <17alma41@gmail.com>
2024-03-21 11:16:43 +0100

    stopreversing

--- ejercicios/stopreversing.js
+++ ejercicios/stopreversing.js
@@ -1,9 +1,25 @@
-//Laz una función que vuelva del revés strings
+//Haz una función que vuelva del revés strings
 //La función debe devolver un string como el original, pero con el orden al inverso
 // Hola => aloH
 // Lorem ipsum => muspi meroL
 //Sólo puedes usar una linea de código
 
-const reverse = () => 
+//const reverse = () => 
+
+/*
+function reverse(str){
+    const array = str.split("")
+    const array_modificado = array.reverse()
+    return array_modificado.join("");
+}
+*/
+
+/*
+function reverse(str){
+    return str.split("").reverse().join("")
+}
+*/
+
+const reverse = (str) => str.split("").reverse().join("")
 
 module.exports = reverse
