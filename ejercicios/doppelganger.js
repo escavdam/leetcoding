d1b21358205ffd34122c9a265a5903658c884ad9
datadiego <juandiegomariscal@gmail.com>
2024-03-21 09:15:15 +0100

    update: nuevos ejercicios

---
+++ ejercicios/doppelganger.js
@@ -0,0 +1,7 @@
+//Haz una función que refleje un string
+//La función debe devolver una copia del string en el orden original, seguido de su copia inversa
+// Hola => HolaaloH
+
+const doppel = (str) =>
+
+module.exports = doppel
---
+++ ejercicios/doublevision.js
@@ -0,0 +1,9 @@
+//Haz una función que duplique caracteres en strings
+//La función debe devolver el mismo string, pero con cada caracter duplicado
+// Hola => HHoollaa
+// Lorem ipsum => LLoorreemm  iippssuumm
+//Sólo puedes usar una linea de código
+
+const doublevision = (str) =>
+
+module.exports = doublevision
